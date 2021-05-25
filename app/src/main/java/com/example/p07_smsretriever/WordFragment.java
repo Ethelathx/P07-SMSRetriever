c841b276461883ec11808b6020794b7a04bf38c8
18016254 <18016254@myrp.edu.sg>
2021-05-25 14:28:18 +0800

    word fragment

--- app/src/main/java/com/example/p07_smsretriever/WordFragment.java
+++ app/src/main/java/com/example/p07_smsretriever/WordFragment.java
@@ -1,21 +1,100 @@
 package com.example.p07_smsretriever;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class WordFragment extends Fragment {
+
+    Button btnRetrieve;
+    EditText etWord;
+    TextView tvResult;
+    public WordFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_word, container, false);
+        btnRetrieve = view.findViewById(R.id.btnRetrieveTextFrag1);
+        etWord = view.findViewById(R.id.etInput1);
+        tvResult = view.findViewById(R.id.tvFrag1);
 
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS);
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_SMS}, 0);
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+
+                String filter = "body LIKE ?";
+                String word = etWord.getText().toString();
+                String[] filterArgs = {"%" + word + "%"};
+
+                String[] reqCols = new String[]{"date","address","body","type"};
+
+                ContentResolver cr = getActivity().getContentResolver();
+
+                Cursor cursor = cr.query(uri, reqCols, filter,filterArgs,null);
+                String smsBody = "";
+                if(cursor.moveToFirst()){
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox: ";
+                        } else {
+                            type = "Sent: ";
+                        }
+                        smsBody += type + " " + address+ "\n at " + date +"\n\"" + body +"\"\n\n";
+                    } while(cursor.moveToNext());
+                }
+                tvResult.setText(smsBody);
+            }
+        });
         return view;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, Do the read SMS as if the btnRetrieve is clicked
+                    btnRetrieve.performClick();
+                } else {
+                    // permission was denied, notify user
+                    Toast.makeText(getActivity(), "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 }
